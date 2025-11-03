e30f64d3dd3c9730ac2774fda50d563fd8832c7b
vvelazquez370 <ve442896@ucf.edu>
2025-11-03 14:10:06 -0500

    Done Task 1, Veronica Velazquez

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //how to define a variable
+    //1. access modifier: public or private
+    //2. data type: int, float, bool, string
+    //3. variable name: camelCase
+    //4. value: optional
+
+    private float playerSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float horizontalScreenLimit = 9.5f;
+    private float minVerticalScreenLimit = -3.25f;
+    private float maxVerticalScreenLimit = 0f;
+
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        playerSpeed = 6f;
+        //This function is called at the start of the game
+        
+    }
+
+    void Update()
+    {
+        //This function is called every frame; 60 frames/second
+        Movement();
+        Shooting();
+
+    }
+
+    void Shooting()
+    {
+        //if the player presses the SPACE key, create a projectile
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+        //Player leaves the screen horizontally
+        if(transform.position.x > horizontalScreenLimit || transform.position.x < -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+        //Player hits top boundary
+        if (transform.position.y > maxVerticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, maxVerticalScreenLimit, 0);
+        }
+        //Player hits bottom boundary
+        if(transform.position.y < minVerticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, minVerticalScreenLimit, 0);
+        }
+    }
+
+}
